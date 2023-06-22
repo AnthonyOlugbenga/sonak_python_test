f79362207db201c0821d5fde3d7b74ea58f22950
Anthony Daniel <ext-daniel.anthony@glaston.net>
2023-06-22 13:24:59 +0300

    third

--- Locators/locators.py
+++ Locators/locators.py
@@ -4,6 +4,7 @@ class Locators():
     username_textbox_id = "txtUsername"
     password_textbox_id = "txtPassword"
     login_button_id = "btnLogin"
+    login_out_id = "btnLogout"
 
     # home page objects
     my_info_module_xpath = '//*[@id="menu_pim_viewMyDetails"]/b'
